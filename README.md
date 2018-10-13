fe1beb4f4dba686860a06866ed7a37021c1e94fd
Brian Schiller <bgschiller@gmail.com>
2018-10-12 21:26:32 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## Markoff Models
+
+In the spirit of the [STUPID SHIT NO ONE NEEDS & TERRIBLE IDEAS HACKATHON](http://www.stupidhackathon.com/) and Develop Denver's Ballmer Peak hackathon, I present: Markoff Models.
+
+They're Markov Models, but sometimes (20% of the time) they throw in a word without regard for the weights.
+
---
+++ markoff.py
@@ -0,0 +1,52 @@
+import re
+import sys
+import random
+import bisect
+import inspect
+import markovify
+from markovify.chain import BEGIN, accumulate
+
+class MarkoffChain(markovify.Chain):
+    def move(self, state):
+        """
+        Given a state, choose the next item at random.
+
+        Sometimes choose arbitrarily, instead of according
+        to the probabilities.
+        """
+        if state == tuple([ BEGIN ] * self.state_size):
+            choices = self.begin_choices
+            cumdist = self.begin_cumdist
+        else:
+            choices, weights = zip(*self.model[state].items())
+            cumdist = list(accumulate(weights))
+        if random.random() < 0.2:
+            # 20% of the time, it works every time...
+            selection = random.choice(choices)
+        r = random.random() * cumdist[-1]
+        selection = choices[bisect.bisect(cumdist, r)]
+        return selection
+
+class MarkoffText(markovify.Text):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.chain.__class__ = MarkoffChain
+
+class MarkoffNewlineText(MarkoffText):
+    def sentence_split(self, text):
+        return re.split(r"\s*\n\s*", text)
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("usage: python markoff.py [filename]", file=sys.stderr)
+        sys.exit(1)
+    fname = sys.argv[1]
+    with open(fname) as f:
+        lines = f.read()
+    if '.newlines.' in fname:
+        model = MarkoffNewlineText(lines)
+    else:
+        model = MarkoffText(lines)
+
+    for _ in range(10):
+        print(model.make_sentence())
